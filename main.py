bf51d82b51f688f269d129ee8a380fe1600134b8
alexeyvershinin <og2001@yandex.ru>
2023-01-29 10:55:45 +0300

    Получил данные и сохранил в json, получил количество доступных страниц

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+import fake_useragent
+import json
+
+ua = fake_useragent.UserAgent()
+
+headers = {
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "X-Is-Ajax-Request": "X-Is-Ajax-Request",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": ua.random
+}
+
+
+def get_data():
+    url = "https://roscarservis.ru/catalog/legkovye/?form_id=catalog_filter_form&filter_mode=params&sort=asc&filter_type=tires&arCatalogFilter_458_1500340406=Y&set_filter=Y&arCatalogFilter_463=668736523&PAGEN_1=1"
+    r = requests.get(url=url, headers=headers)
+
+    # сохраним полученные данные в json
+    with open('r.json', 'w', encoding='utf-8') as file:
+        json.dump(r.json(), file, indent=4, ensure_ascii=False)
+
+    # получим количество доступных страниц
+    pages_count = r.json()["pagesCount"]
+    print(pages_count)
+
+
+def main():
+    get_data()
+
+
+if __name__ == '__main__':
+    main()
